b1bc2fcd6a6d32f81d96a2687c3b78bbe0bda609
Molibar <christer.brannstrom@gmail.com>
2017-01-02 11:00:54 +0000

    Created MVC5 project

---
+++ VisualStudio/Mvc5Playground.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5Playground.Startup))]
+namespace Mvc5Playground
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
